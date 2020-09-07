2730867efded27b1a00f44317f3bc1fe51df3705
Jon Mitts <mitts.3@gmail.com>
2020-09-07 17:22:56 -0400

    Added Package.swift.

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.0
+//
+//  Package.swift
+//  CorePlot
+//
+//  Created by Jon Mitts on 9/7/20.
+//
+
+import PackageDescription
+
+let package = Package(name: "CorePlot",
+                      platforms: [.macOS(.v10_10),
+                                  .iOS(.v9),
+                                  .tvOS(.v9),
+                                  .watchOS(.v2)],
+                      products: [.library(name: "CorePlot",
+                                          targets: ["CorePlot"])],
+                      targets: [.target(name: "CorePlot",
+                                        path: "CorePlot",
+                                        publicHeadersPath: "")])
