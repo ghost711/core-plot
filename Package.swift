16ad7b15bb1757631d31c7de146a406183b7e42d
Jon Mitts <mitts.3@gmail.com>
2020-09-07 18:08:49 -0400

    Alt method.

--- Package.swift
+++ Package.swift
@@ -8,6 +8,20 @@
 
 import PackageDescription
 
+//let package = Package(name: "CorePlot",
+//    platforms: [.macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)],
+//    products: [
+//        .library(name: "CorePlot_iOS", targets: ["CorePlot_iOS"]),
+//        .library(name: "CorePlot_OSX", targets: ["CorePlot_OSX"])
+//    ],
+//    targets: [
+//        .target(name: "CorePlot_iOS", path: "CorePlot_iOS",
+//                                      publicHeadersPath: "CorePlot.h"),
+//        .target(name: "CorePlot_OSX", path: "CorePlot_OSX",
+//                                      publicHeadersPath: "CorePlot.h")
+//    ]
+//)
+
 let package = Package(name: "CorePlot",
     platforms: [.macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)],
     products: [
@@ -15,9 +29,12 @@ let package = Package(name: "CorePlot",
         .library(name: "CorePlot_OSX", targets: ["CorePlot_OSX"])
     ],
     targets: [
-        .target(name: "CorePlot_iOS", path: "CorePlot_iOS"),
-        .target(name: "CorePlot_OSX", path: "CorePlot_OSX")
-    ] 
+        .target(name: "CorePlotCore", path: "framework/Source"),
+        .target(name: "CorePlot_iOS", path: "framework/iPhoneOnly",
+                dependencies: ["CorePlotCore"], publicHeadersPath: ""),
+        .target(name: "CorePlot_OSX", path: "framework/MacOnly",
+                dependencies: ["CorePlotCore"], publicHeadersPath: "")
+    ]
 )
                       //targets: [.target(name: "CorePlot",
                       //                path: "CorePlot",
