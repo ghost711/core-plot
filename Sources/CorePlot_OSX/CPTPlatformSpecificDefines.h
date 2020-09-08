5267abd869e7bf4187135d833da4f303639fc50e
Jon Mitts <mitts.3@gmail.com>
2020-09-08 14:33:52 -0400

    Hide OS X only types.

--- Sources/CorePlot_OSX/CPTPlatformSpecificDefines.h
+++ Sources/CorePlot_OSX/CPTPlatformSpecificDefines.h
@@ -1,6 +1,10 @@
 /// @file
 
+#import <TargetConditionals.h>
+
+#if !TARGET_OS_SIMULATOR && !TARGET_OS_IPHONE 
 typedef NSColor CPTNativeColor; ///< Platform-native color.
 typedef NSImage CPTNativeImage; ///< Platform-native image format.
 typedef NSEvent CPTNativeEvent; ///< Platform-native OS event.
 typedef NSFont  CPTNativeFont;  ///< Platform-native font.
+#endif
---
+++ Sources/CorePlot_OSX/SpmImporter.h
@@ -0,0 +1,36 @@
+//
+//  SpmImporter.h
+//  CorePlot
+//
+//  Created by Jon Mitts on 9/8/20.
+//
+
+/** Takes the place of the pch for Swift Package Manager. */
+
+#ifdef __OBJC__
+    #import <TargetConditionals.h>
+    //@import Darwin.TargetConditionals;
+
+    #if TARGET_OS_SIMULATOR || TARGET_OS_IPHONE || TARGET_OS_TV
+        #import <Foundation/Foundation.h>
+        #import <UIKit/UIKit.h>
+        //@import Foundation;
+        //@import UIKit;
+    #else
+        #import <Cocoa/Cocoa.h>
+        //@import Cocoa;
+
+        #ifndef MAC_OS_X_VERSION_10_6
+        #define MAC_OS_X_VERSION_10_6 1060
+        #endif
+
+        #ifndef MAC_OS_X_VERSION_10_7
+        #define MAC_OS_X_VERSION_10_7 1070
+        #endif
+
+        #ifndef MAC_OS_X_VERSION_10_8
+        #define MAC_OS_X_VERSION_10_8 1080
+        #endif
+    #endif
+#endif
+
