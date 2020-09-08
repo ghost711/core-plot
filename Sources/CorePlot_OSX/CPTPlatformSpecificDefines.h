cd91b583b2de34499e946fbf21c4a0419b338104
Jon Mitts <mitts.3@gmail.com>
2020-09-08 14:51:47 -0400

    .

--- Sources/CorePlot_OSX/CPTPlatformSpecificDefines.h
+++ Sources/CorePlot_OSX/CPTPlatformSpecificDefines.h
@@ -2,9 +2,14 @@
 
 #import <TargetConditionals.h>
 
-#if !TARGET_OS_SIMULATOR && !TARGET_OS_IPHONE 
-typedef NSColor CPTNativeColor; ///< Platform-native color.
-typedef NSImage CPTNativeImage; ///< Platform-native image format.
-typedef NSEvent CPTNativeEvent; ///< Platform-native OS event.
-typedef NSFont  CPTNativeFont;  ///< Platform-native font.
+#if TARGET_OS_SIMULATOR || TARGET_OS_IPHONE
+    typedef UIColor CPTNativeColor; ///< Platform-native color.
+    typedef UIImage CPTNativeImage; ///< Platform-native image format.
+    typedef UIEvent CPTNativeEvent; ///< Platform-native OS event.
+    typedef UIFont  CPTNativeFont;  ///< Platform-native font.
+#else
+    typedef NSColor CPTNativeColor; ///< Platform-native color.
+    typedef NSImage CPTNativeImage; ///< Platform-native image format.
+    typedef NSEvent CPTNativeEvent; ///< Platform-native OS event.
+    typedef NSFont  CPTNativeFont;  ///< Platform-native font.
 #endif
