78e469bc1ca08c5711333c33e574bfdafaa08f9f
Jon Mitts <mitts.3@gmail.com>
2020-09-08 15:08:03 -0400

    .

--- Sources/CorePlot_iOS/CPTTestCase.h
+++
@@ -1,9 +0,0 @@
-#import "SpmImporter.h"
-#import <XCTest/XCTest.h>
-
-@interface CPTTestCase : XCTestCase
-
--(nullable id)archiveRoundTrip:(nonnull id)object toClass:(nonnull Class)archiveClass;
--(nullable id)archiveRoundTrip:(nonnull id)object;
-
-@end
